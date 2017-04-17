04f0631e23cae102a11ae36873e50ec40e3f60fc
Hank's Mac <hankhplee@gmail.com>
2017-04-17 16:03:57 +0800

    bonus

--- lab6/temp_sensor.py
+++ lab6/temp_sensor.py
@@ -2,13 +2,12 @@ import Adafruit_DHT
 import os
 import pygame
 import time
-
+import sys
 
 class pyscope :
     screen = None;
 
     def __init__(self):
-
 
         "Ininitializes a new pygame screen using the framebuffer"
         disp_no = os.getenv("DISPLAY")
@@ -74,8 +73,17 @@ class pyscope :
 
 
 # Create an instance of the PyScope class
-sensor = Adafruit_DHT.DHT22
-pin = 'P8_11'
+sensor_args = { '11': Adafruit_DHT.DHT11,
+                '22': Adafruit_DHT.DHT22,
+                '2302': Adafruit_DHT.AM2302 }
+if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+    sensor = sensor_args[sys.argv[1]]
+    pin = sys.argv[2]
+else:
+    print('usage: sudo ./Adafruit_DHT.py [11|22|2302] GPIOpin#')
+    print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
+    sys.exit(1)
+
 scope = pyscope()
 scope.display()
 time.sleep(10)
