f4b5d99d765c5c63faabe2acf750f638a323a44c
Hank's Mac <hankhplee@gmail.com>
2017-05-15 13:44:51 +0800

    lab9-2-1

---
+++ lab9-2/01.py
@@ -0,0 +1,16 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+time.sleep(2)
+camera.capture(rawCapture, format="bgr");
+image = rawCapture.array
+
+cv2.circle(image,(50,50),10,(255,0,0),2)
+cv2.rectangle(image,(20,20),(60,60),(0,0,255),3)
+
+cv2.imshow("Fuck You",image)
+cv2.waitKey(0)
