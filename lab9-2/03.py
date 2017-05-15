005563258cca5281e596400417f061c127d0e1b3
dimension4tw <hankhplee@gmail.com>
2017-05-15 06:38:11 +0000

    update

--- lab9-2/03.py
+++ lab9-2/03.py
@@ -12,14 +12,13 @@ time.sleep(2)
 
 for frame in camera.capture_continuous(rawCapture,format="bgr",use_video_port=True):
     image = frame.array
-    image = cv2.imread('image.jpg')
     face = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     face_pos = face.detectMultiScale(gray, 1.1, 5)
     for (x,y,w,h) in face_pos:
         cv2.rectangle(image,(x,y),(x+w,y+h),(255,2,2),2)
     cv2.imshow('result',image)
-    cv2.waitKey(1) & 0xFF
+    key = cv2.waitKey(1) & 0xFF
     rawCapture.truncate(0)
-    if key == ord("q");
-    break
+    if key == ord("q"):
+        break
