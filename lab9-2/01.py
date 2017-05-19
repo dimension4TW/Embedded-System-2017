59f1d8625c982341e3a90f35e6a5e40d525e07e9
dimension4tw <hankhplee@gmail.com>
2017-05-19 15:54:15 +0000

    update

--- lab9-2/01.py
+++ lab9-2/01.py
@@ -12,5 +12,6 @@ image = rawCapture.array
 cv2.circle(image,(50,50),10,(255,0,0),2)
 cv2.rectangle(image,(20,20),(60,60),(0,0,255),3)
 
-cv2.imshow("Fuck You",image)
+#cv2.imshow("Fuck You",image)
+cv2.imwrite("result_p1.jpg",image)
 cv2.waitKey(0)
